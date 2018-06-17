6aeb2de0d040f54ddb6b9c50064a584d33c76f76
IsaacShelton <isaac.a.shelton@gmail.com>
2018-06-17 09:10:00 -0500

    Added pragma project_name <name> & pragma optimization <level> directives

--- src/pragma.c
+++ src/pragma.c
@@ -1,5 +1,6 @@
 
 #include "pragma.h"
+#include "filename.h"
 
 int parse_pragma(parse_ctx_t *ctx){
     // pragma <option> ...
@@ -43,9 +44,46 @@ int parse_pragma(parse_ctx_t *ctx){
             if(compiler_create_package(ctx->compiler, ctx->object) == 0) ctx->compiler->result_flags |= COMPILER_RESULT_SUCCESS;
             return 1;
         }
+    } else if(strcmp(option, "project_name") == 0){
+        if(tokens[++(*i)].id != TOKEN_CSTRING && tokens[*i].id != TOKEN_STRING){
+            compiler_panic(ctx->compiler, ctx->tokenlist->sources[*i - 1], "Expected string containing project name after 'pragma project_name'");
+            return 1;
+        }
+
+        free(ctx->compiler->output_filename);
+        ctx->compiler->output_filename = filename_local(ctx->object->filename, (char*) tokens[*i].data);
+        return 0;
+    } else if(strcmp(option, "optimization") == 0){
+        if(tokens[++(*i)].id != TOKEN_WORD){
+            compiler_panic(ctx->compiler, ctx->tokenlist->sources[*i - 1], "Expected optimization level after 'pragma optimization'");
+            printf("Possible levels are: none, less, normal or aggressive\n");
+            return 1;
+        }
+
+        const char *level = (char*) tokens[*i].data;
+
+        if(strcmp(level, "none") == 0){
+            ctx->compiler->optimization = OPTIMIZATION_NONE;
+            return 0;
+        } else if(strcmp(level, "less") == 0){
+            ctx->compiler->optimization = OPTIMIZATION_LESS;
+            return 0;
+        } else if(strcmp(level, "normal") == 0){
+            ctx->compiler->optimization = OPTIMIZATION_DEFAULT;
+            return 0;
+        } else if(strcmp(level, "aggressive") == 0){
+            ctx->compiler->optimization = OPTIMIZATION_AGGRESSIVE;
+            return 0;
+        } else {
+            // Invalid optimiztaion level
+            compiler_panic(ctx->compiler, ctx->tokenlist->sources[*i], "Invalid optimization level after 'pragma optimization'");
+            printf("Possible levels are: none, less, normal or aggressive\n");
+            return 1;
+        }
     } else {
         compiler_panicf(ctx->compiler, ctx->tokenlist->sources[*i - 1], "Unrecognized pragma option '%s'", option);
         return 1;
     }
+
     return 0;
 }
