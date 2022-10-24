bd2d2f36660c511c9da38ef734f588d6d420db19
IsaacShelton <isaac.a.shelton@gmail.com>
2022-10-23 22:27:45 -0500

    Updated object.c to work again with insight API

--- src/DRVR/object.c
+++ src/DRVR/object.c
@@ -6,6 +6,7 @@ void object_init_ast(object_t *object, unsigned int cross_compile_for){
     object->compilation_stage = COMPILATION_STAGE_AST;
 }
 
+#ifndef ADEPT_INSIGHT_BUILD
 void object_create_module(object_t *object){
     ast_t *ast = &object->ast;
     ir_module_t *module = &object->ir_module;
@@ -16,3 +17,4 @@ void object_create_module(object_t *object){
     // Advance compilation stage
     object->compilation_stage = COMPILATION_STAGE_IR_MODULE;
 }
+#endif
