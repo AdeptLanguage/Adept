785613d04c0ecefc871b967f284db63c7344447b
IsaacShelton <isaac.a.shelton@gmail.com>
2024-02-08 18:31:53 -0600

    Debug weird GitHub actions anomaly. Try writing file since seems to fail?

--- tests/e2e/e2e-runner.py
+++ tests/e2e/e2e-runner.py
@@ -56,7 +56,7 @@ def run_all_tests():
     # Close file
     text_file.close()
     print(os.listdir("src/anonymous_enums"))
-    subprocess.run([executable, join(src_dir, "anonymous_enums/x.adept", "-o", "src/anonymous_enums/x")]) 
+    subprocess.run([executable, join(src_dir, "anonymous_enums/x.adept"), "-o", "src/anonymous_enums/x", "-e"]) 
     print(os.listdir("src/anonymous_enums"))
 
     print(os.listdir("src"))
