3e5b814af8450aab887c96243053bf08fcf44203
IsaacShelton <isaac.a.shelton@gmail.com>
2025-11-09 14:55:28 -0600

    Minor set of bug fixes

--- src/AST/ast_layout.c
+++ src/AST/ast_layout.c
@@ -408,9 +408,6 @@ successful_t ast_layout_endpoint_init_with(ast_layout_endpoint_t *endpoint, uint
     // Set end index, so we can determine the length of this endpoint's indices
     if(length != AST_LAYOUT_MAX_DEPTH) endpoint->indices[length] = AST_LAYOUT_ENDPOINT_END_INDEX;
 
-    // Silence warnings about copying uninitialized data
-    memset(endpoint->indices, 0, sizeof(uint16_t) * AST_LAYOUT_MAX_DEPTH);
-
     memcpy(endpoint->indices, indices, sizeof(uint16_t) * length);
     return true;
 }
